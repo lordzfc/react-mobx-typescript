df23fde6b1ca76d1dbdcdc958f9a65df955c8e27
Pawel Galwa <pawelgalwa@Pawels-MacBook-Pro.local>
2018-04-02 17:38:34 +0200

    cleaned up from unnecessary console.logs

--- app/components/hello/hello.tsx
+++ app/components/hello/hello.tsx
@@ -6,7 +6,6 @@ export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        console.log(styles.hello.toString())
         return <h1 className={styles.hello}>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
